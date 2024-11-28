0514a5e05717de5ff87aac9cfd6f697172321262
Andrey Chetvergov <andreychetvergov@MacBook-Pro-Andrey.local>
2024-11-28 10:45:19 +0300

    add src

---
+++ src/leaderboard.py
@@ -0,0 +1,20 @@
+# src/leaderboard.py
+
+
+def update_leaderboard(model_name, accuracy, f1_score, levenstein_score):
+    """
+    Функция для обновления лидерборда с результатами модели.
+
+    :param model_name: имя модели.
+    :param accuracy: точность модели.
+    :param f1_score: F1-метрика модели.
+    :param levenstein_score: расстояние Левенштейна.
+    """
+    pass
+
+
+def display_leaderboard():
+    """
+    Функция для отображения текущего лидерборда.
+    """
+    pass
