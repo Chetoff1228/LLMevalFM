7d2df51ef0d983a47c42705af47d8c552f3d5581
Андрей Четвергов <chet-off1228@yandex.ru>
2024-12-29 19:07:25 +0300

    Remove unnecessary files, update .gitignore and other dependencies

---
+++ src/config.py
@@ -0,0 +1,144 @@
+from typing import Final
+
+# Questions Dataset
+QUESTIONS_DATASET_COLUMNS: Final[list[str]] = [
+    "id",  # Unique identifier for each question entry.
+    "subject",  # The subject or topic to which the question belongs.
+    "type",  # The type of question (e.g., "открытый ответ").
+    "instruction",  # Instructions on how to answer the question.
+    "task",  # The specific task or action required from the respondent.
+    "text",  # The main content of the question or prompt.
+    "option_1",  # The first option for multiple-choice answers.
+    "option_2",  # The second option for multiple-choice answers.
+    "option_3",  # The third option for multiple-choice answers.
+    "option_4",  # The fourth option for multiple-choice answers.
+    "option_5",  # The fifth option for multiple-choice answers.
+    "option_6",  # The sixth option for multiple-choice answers.
+    "option_7",  # The seventh option for multiple-choice answers.
+    "option_8",  # The eighth option for multiple-choice answers.
+    "option_9",  # The ninth option for multiple-choice answers.
+    "outputs",  # The expected or correct output for the question.
+    "source",  # The source from which the question was derived.
+    "comment",  # Any additional comments or notes related to the question.
+    "provoc_score",  # The provocation score associated with the question, often used for evaluation.
+]
+
+# Columns
+INSTRUCTION_COLUMN: Final[str] = "instruction"
+INPUTS_COLUMN: Final[str] = "inputs"
+TASK_COLUMN: Final[str] = "task"
+TEXT_COLUMN: Final[str] = "text"
+OPTIONS_COLUMN: Final[str] = "options"
+OPTION_SUBCOLUMN_TEMPLATE: Final[str] = "option_{}"
+META_COLUMN: Final[str] = "meta"
+ID_COLUMN: Final[str] = "id"
+SUBJECT_COLUMN: Final[str] = "subject"
+TYPE_COLUMN: Final[str] = "type"
+SOURCE_COLUMN: Final[str] = "source"
+COMMENT_COLUMN: Final[str] = "comment"
+PROVOC_SCORE_COLUMN: Final[str] = "provoc_score"
+
+# Metrics
+MODEL_COLUMN: Final = "model"
+REAL_ANSWER_COLUMN: Final[str] = "outputs"
+MODEL_ANSWER_COLUMN: Final[str] = "response"
+
+OPEN_QUESTION_FLAG_COLUMN: Final[str] = "open_question_flag"
+ONLY_NUMBERS_MODEL_ANSWER_COLUMN: Final[str] = "only_numbers_response"
+
+OPEN_QUESTION_TYPE_NAME: Final[str] = "open_question"
+NOT_OPEN_QUESTION_TYPE_NAME: Final[str] = "not_open_question"
+
+OPEN_QUESTION_VALUE: Final[str] = "открытый ответ"
+SINGLE_CHOICE: Final[str] = "выбор ответа (один)"
+MULTI_CHOICE: Final[str] = "выбор ответа (мультивыбор)"
+MATCHING: Final[str] = "установление соответствия"
+SEQUENCE: Final[str] = "указание последовательности"
+
+EXACT_MATCH_COLUMN: Final[str] = "exact_match"
+LEVENSHTEIN_RATIO_COLUMN: Final[str] = "levenshtein_ratio"
+F1_SCORE_COLUMN: Final[str] = "f1_score"
+IS_SUBSTRING_COLUMN: Final[str] = "is_substring"
+PARTIALLY_MATCH_COLUMN: Final[str] = "partially_match"
+
+QUESTION_TYPES_NAMING = {
+    OPEN_QUESTION_TYPE_NAME: "Open",
+    NOT_OPEN_QUESTION_TYPE_NAME: "Not open",
+}
+
+LEADERBOARD_SHEET_NAME = "Leaderboard"
+TYPE_OF_QUESTION_SHEET_NAME = "Type of question"
+SUBJECT_SHEET_NAME = "Subject"
+PROVOCATIVENESS_SHEET_NAME = "Provocativeness"
+
+KEYS_NAMING = {
+    TYPE_COLUMN: TYPE_OF_QUESTION_SHEET_NAME,
+    SUBJECT_COLUMN: SUBJECT_SHEET_NAME,
+    PROVOC_SCORE_COLUMN: PROVOCATIVENESS_SHEET_NAME,
+}
+
+SUBJECTS_NAMING = {
+    "Обществознание": "Social studies",
+    "История": "History",
+    "География": "Geography",
+    "Политология": "Political science",
+}
+
+QUESTION_VALUES_NAMING = {
+    OPEN_QUESTION_VALUE: "Open answer",
+    SINGLE_CHOICE: "Single choice",
+    MULTI_CHOICE: "Multiple choice",
+    MATCHING: "Matching",
+    SEQUENCE: "Sequence",
+}
+
+PROVOCATIVENESS_NAMING = {"1": "Low", "2": "Medium", "3": "High"}
+
+COMBINED_VALUES_NAMING = {
+    **SUBJECTS_NAMING,
+    **QUESTION_VALUES_NAMING,
+    **PROVOCATIVENESS_NAMING,
+}
+
+METRICS_NAMING = {
+    EXACT_MATCH_COLUMN: "EM",
+    LEVENSHTEIN_RATIO_COLUMN: "LR",
+    F1_SCORE_COLUMN: "F1",
+    IS_SUBSTRING_COLUMN: "IS",
+    PARTIALLY_MATCH_COLUMN: "PM",
+}
+
+AGGFUNC: Final[str] = "mean"
+
+# DataLoader
+REPO_ID: Final[str] = "RANEPA-ai/SLAVA-OpenData-2800-v1"
+REPO_TYPE: Final[str] = "dataset"
+OPEN_DATASET_FILENAME: Final[str] = "open_questions_dataset.jsonl"
+REQUIRED_COLUMNS: Final[list[str]] = [
+    INSTRUCTION_COLUMN,
+    INPUTS_COLUMN,
+    REAL_ANSWER_COLUMN,
+    META_COLUMN,
+]
+
+# Pivot tables
+OPEN_QUESTION_VALUES_FOR_PIVOT_TABLES: Final[list[str]] = [
+    EXACT_MATCH_COLUMN,
+    LEVENSHTEIN_RATIO_COLUMN,
+    F1_SCORE_COLUMN,
+]
+NOT_OPEN_QUESTION_VALUES_FOR_PIVOT_TABLES: Final[list[str]] = [
+    EXACT_MATCH_COLUMN,
+    IS_SUBSTRING_COLUMN,
+    PARTIALLY_MATCH_COLUMN,
+]
+
+# Models
+MODELS_TYPES: Final[tuple[str]] = [
+    "gigachat",
+    "huggingface",
+    "ollama",
+    "openai",
+    "yandexgpt",
+]
+DEVICE: Final[int] = 0
---
+++ src/modules/data_loader.py
@@ -0,0 +1,42 @@
+import logging
+
+import pandas as pd
+from huggingface_hub import hf_hub_download
+
+from src.config import OPEN_DATASET_FILENAME, REPO_ID, REPO_TYPE, REQUIRED_COLUMNS
+
+
+class DataLoader:
+
+    def __init__(
+        self,
+        repo_id: str = REPO_ID,
+        filename: str = OPEN_DATASET_FILENAME,
+    ):
+        self.repo_id = repo_id
+        self.filename = filename
+
+    def load_data(self, dataset_path: str = None) -> pd.DataFrame:
+        # Local JSONL
+        if dataset_path:
+            logging.info(f"Dataset loaded from local by path - {dataset_path}")
+            data = pd.read_json(dataset_path, lines=True)
+        # HuggingFace JSONL
+        elif self.repo_id and self.filename:
+            logging.info(f"Dataset loaded from HuggingFace by path - {self.repo_id}/{self.filename}")
+            data = pd.read_json(
+                hf_hub_download(repo_id=self.repo_id, filename=self.filename, repo_type=REPO_TYPE),
+                lines=True,
+            )
+        else:
+            raise ValueError("The parameters for loading data are not specified")
+
+        # Validate the dataset format
+        if not self.validate_format(data):
+            raise ValueError("The uploaded dataset does not match the required format")
+        else:
+            logging.info("The initial validation has been completed")
+        return data
+
+    def validate_format(self, data: pd.DataFrame) -> bool:
+        return all(column in data.columns for column in REQUIRED_COLUMNS)
